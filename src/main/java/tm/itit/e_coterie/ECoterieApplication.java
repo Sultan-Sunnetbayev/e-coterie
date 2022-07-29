f997f1fad4c4377c9a29bcde2c6189d6b7762e2d
Sultan-Sunnetbayev <sunnetbaev@mail.ru>
2022-07-29 16:45:41 +0500

    initial commit

---
+++ src/main/java/tm/itit/e_coterie/ECoterieApplication.java
@@ -0,0 +1,13 @@
+package tm.itit.e_coterie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECoterieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECoterieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tm/itit/e_coterie/ECoterieApplicationTests.java
@@ -0,0 +1,13 @@
+package tm.itit.e_coterie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECoterieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
