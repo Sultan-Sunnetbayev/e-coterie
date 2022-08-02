123aafef128850890fd548f8a82eeac619b819a9
Sultan-Sunnetbayev <sunnetbaev@mail.ru>
2022-08-02 14:46:44 +0500

    Added Services And Configuration
    * added SecurityConfig
    * added interface UserService
    * added class UserServiceImpl

---
+++ src/main/java/tm/itit/e_coterie/services/UserService.java
@@ -0,0 +1,4 @@
+package tm.itit.e_coterie.services;
+
+public interface UserService {
+}
---
+++ src/main/java/tm/itit/e_coterie/services/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package tm.itit.e_coterie.services;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import tm.itit.e_coterie.daos.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+}
